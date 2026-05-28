9d049d4afbc189f92064c105b096451d5e502f50
Victor <victorezaki19@gmail.com>
2026-05-28 00:04:12 -0300

    add database.js

--- Back/src/config/database.js
+++ Back/src/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(
     process.env.DB_NAME || 'aperture_science',
     process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '50ft4@t1',
+    process.env.DB_PASSWORD || '',
     {
         host: process.env.DB_HOST || 'localhost',
         dialect: 'mysql',
